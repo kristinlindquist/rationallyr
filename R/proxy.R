89780d8c55656090cc239dc748b9648c16ccdf87
Kristin Lindquist <lindquist.kristin@gmail.com>
2018-10-13 13:02:42 -0600

    tweak

--- R/proxy.R
+++ R/proxy.R
@@ -1,4 +1,8 @@
-library(pwr); library(PowerTOST);
+library(devtools);
+install_github("llamallamagirl/PowerTOST");
+library(pwr);
+library(PowerTOST);
+
 .proxy <- function(vars, powerMethod) {
     result <- list;
     for (o in list(vars)) { result <- c(list(powerMethod), result); };
