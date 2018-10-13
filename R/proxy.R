09ae444f31f24620532113b34f132400604f6b8f
Kristin Lindquist <lindquist.kristin@gmail.com>
2018-10-13 15:03:45 -0600

    tweak

--- R/proxy.R
+++ R/proxy.R
@@ -1,10 +1,11 @@
 library(devtools)
+library(jsonlite)
 install_github("llamallamagirl/PowerTOST")
 
 proxy <- function(method, data) {
     result <- list
     for (o in list(data)) {
-      result <- c(list(get(method)(o), result))
+      result <- c(list(get(method)(fromJSON(o)), result))
     }
     return(result)
 }
