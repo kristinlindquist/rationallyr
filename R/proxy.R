917fa5c6f3a22fc7ba2226e7d62487e2579ea587
Kristin Lindquist <lindquist.kristin@gmail.com>
2018-10-14 15:32:36 -0600

    tweak

--- R/proxy.R
+++ R/proxy.R
@@ -7,12 +7,12 @@ proxy <- function(method, data) {
     result <- list
     result <- list(lapply(
       split(data, 1:nrow(data)),
-      function(r) docall(method, r)
+      function(r) c(docall(method, r))
     ), result)
-    return(result)
+    return(unname(result))
 }
 
 docall <- function(method, row) {
-  power <- do.call(method, row)
-  return(c(row, c(power=power)))
+  row[["power"]] <- do.call(method, row)
+  return(row)
 }
