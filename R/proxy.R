435a05b8d6ad33b3c1bbc8a4533c7b4ef50d0ae9
Kristin Lindquist <lindquist.kristin@gmail.com>
2018-10-14 20:13:17 -0600

    tweak

--- R/proxy.R
+++ R/proxy.R
@@ -1,15 +1,16 @@
 library(jsonlite)
 library(PowerTOST)
 
-proxy <- function(method, data) {
+proxy <- function(object) {
     result <- lapply(
-      split(data, 1:nrow(data)),
-      function(r) unbox(docall(method, r))
+      split(data, 1:nrow(object$data)),
+      function(r) unbox(docall(object$method, r))
     )
     return(unname(result))
 }
 
 docall <- function(method, row) {
+  row[!grepl("method",unlist(row))]
   row[["power"]] <- do.call(method, row)
   return(row)
 }
