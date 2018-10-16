11333f3dadc73acea62e9544af4e3cc99658b9e2
Kristin Lindquist <lindquist.kristin@gmail.com>
2018-10-15 22:07:40 -0600

    new method for powerlmm

--- R/proxy.R
+++ R/proxy.R
@@ -21,7 +21,7 @@ docall <- function(method, row) {
   return(row)
 }
 
-powerlmmProxy <- function(method, data, ...) {
-  p <- proxy("study_parameters", data.params)
-  return(proxy("get_power", c(data.data, p = p)))
+powerlmmProxy <- function(params, data, ...) {
+  p <- proxy("study_parameters", params)
+  return(proxy("get_power", c(data, p = p)))
 }
