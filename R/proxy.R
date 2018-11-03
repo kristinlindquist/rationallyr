5a802fa18d7837e72f667084956b8558f57b992c
Kristin Lindquist <lindquist.kristin@gmail.com>
2018-11-03 16:40:38 -0600

    fix for powerlmm

--- R/proxy.R
+++ R/proxy.R
@@ -1,20 +1,22 @@
 proxy <- function(method, data) {
     result <- lapply(
       split(data, 1:nrow(data)),
-      function(r) unbox(docall(method, parseFun(r)))
+      function(r) unbox(docall(method, r))
     )
     return(unname(result))
 }
 
 parseFun <- function(r) {
   for (key in names(r)) {
-    if (grepl("function", key)) r[key] = parse(text = r[key])
+    if (grepl("function", key)) {
+      r[key] = parse(text = r[key])
+    }
   }
   return(r)
 }
 
 docall <- function(method, row) {
-  power <- do.call(method, row)
+  power <- do.call(method, parseFun(row))
   if (typeof(power) == 'list' || typeof(power) == 'S4') {
     row[["power"]] <- power$power
   } else {
@@ -23,7 +25,7 @@ docall <- function(method, row) {
   return(row)
 }
 
-powerlmmProxy <- function(params, data, ...) {
-  studyParams <- do.call("study_parameters", params)
-  return(do.call("get_power", list(studyParams, data)))
+powerlmmProxy <- function(...) {
+  studyParams <- do.call("study_parameters", as.list(match.call()))
+  return(do.call("get_power", list(studyParams)))
 }
