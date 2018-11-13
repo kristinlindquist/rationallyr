b7dac2cdffcfee60afe492c5ed4ab7f9fb01892d
Kristin Lindquist <lindquist.kristin@gmail.com>
2018-11-13 09:15:03 -0700

    adding andrew gelman typeS/typeM method

--- R/proxy.R
+++ R/proxy.R
@@ -38,3 +38,15 @@ powerlmmProxy <- function(...) {
   studyParams <- do.call("study_parameters", as.list(match.call()[-1]))
   return(do.call("get_power", list(studyParams)))
 }
+
+retrodesign <- function(A, s, alpha=.05, df=Inf, n.sims=10000){
+  z <- qt(1-alpha/2, df)
+  p.hi <- 1 - pt(z-A/s, df)
+  p.lo <- pt(-z-A/s, df)
+  power <- p.hi + p.lo
+  typeS <- p.lo/power
+  estimate <- A + s*rt(n.sims,df)
+  significant <- abs(estimate) > s*z
+  exaggeration <- mean(abs(estimate)[significant])/A
+  return(list(power=power, typeS=typeS, exaggeration=exaggeration))
+}
