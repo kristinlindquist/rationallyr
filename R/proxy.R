b9d6524315aa222f65e7df65a107df56d2341c8d
Kristin Lindquist <lindquist.kristin@gmail.com>
2020-04-17 14:39:29 -0600

    cleanup

--- R/proxy.R
+++ R/proxy.R
@@ -55,13 +55,13 @@ gelman <- function(d, se, alpha=0.05, df=Inf, n.sims=10000) {
 }
 
 retrodesign <- function(A, s, alpha=0.05, df=Inf, n.sims=10000){
-  z <- qt(1-alpha/2, df)
-  p.hi <- 1 - pt(z-A/s, df)
-  p.lo <- pt(-z-A/s, df)
+  z <- qt(1 - alpha / 2, df)
+  p.hi <- 1 - pt(z - A / s, df)
+  p.lo <- pt(-z - A / s, df)
   power <- p.hi + p.lo
-  typeS <- p.lo/power
-  estimate <- A + s*rt(n.sims,df)
-  significant <- abs(estimate) > s*z
-  exaggeration <- mean(abs(estimate)[significant])/A
+  typeS <- p.lo / power
+  estimate <- A + s * rt(n.sims,df)
+  significant <- abs(estimate) > s * z
+  exaggeration <- mean(abs(estimate)[significant]) / A
   return(list(power=power, typeS=typeS, exaggeration=exaggeration, dRep=mean(estimate)))
 }
